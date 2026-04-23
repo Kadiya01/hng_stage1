d93b694fa2e6273d1e420bf2fee91347eae041d0
Jamilu Abdullahi Kadiya <92039316+Kadiya01@users.noreply.github.com>
2026-04-23 20:19:25 +0100

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,28 +1,28 @@
-from sqlalchemy import Column, String, Integer, Float, DateTime
-from datetime import datetime, timezone
-from api.db_setup import Base 
+#from sqlalchemy import Column, String, Integer, Float, DateTime
+#from datetime import datetime, timezone
+#from api.db_setup import Base 
 
-class Profile(Base):
-    __tablename__ = "profiles"
+#class Profile(Base):
+ #   __tablename__ = "profiles"
     
     # Primary Key
-    id = Column(String, primary_key=True, index=True) 
+  #  id = Column(String, primary_key=True, index=True) 
     
     # User Details
-    name = Column(String, unique=True, index=True, nullable=False)
-    gender = Column(String)
-    gender_probability = Column(Float)
+   # name = Column(String, unique=True, index=True, nullable=False)
+    #gender = Column(String)
+    #gender_probability = Column(Float)
     
     # Age Details
-    age = Column(Integer)
-    age_group = Column(String) 
+    #age = Column(Integer)
+    #age_group = Column(String) 
     
     # Country Details
-    country_id = Column(String(2)) 
-    country_name = Column(String)
-    country_probability = Column(Float)
+    #country_id = Column(String(2)) 
+    #country_name = Column(String)
+    #country_probability = Column(Float)
     
     # Timestamp
-    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utcnow))
+    #created_at = Column(DateTime, default=lambda: datetime.now(timezone.utcnow))
 
   
